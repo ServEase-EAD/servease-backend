208829d756e617c21457e362abf36e85c0119883
Hasith Gamlath <hasithgamlath327@gmail.com>
2025-11-05 01:05:21 +0530

    Refactor Docker Compose and Nginx configurations; update CORS settings in employee service

--- employee-service/employee_service/settings.py
+++ employee-service/employee_service/settings.py
@@ -59,9 +59,12 @@ MIDDLEWARE = [
 ]
 
 # CORS settings
-# Disabled - nginx handles CORS
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 # REST Framework settings
 REST_FRAMEWORK = {
